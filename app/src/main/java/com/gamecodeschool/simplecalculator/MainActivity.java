197c75ad65804ed6f7a055edf9070286b01b9d7c
paulinemalova <perleebrown@gmail.com>
2019-05-31 13:44:46 +0300

    It can now accept floats not only integers

--- app/src/main/java/com/gamecodeschool/simplecalculator/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/simplecalculator/MainActivity.java
@@ -18,19 +18,40 @@ public class MainActivity extends AppCompatActivity {
         EditText e1 = (EditText)findViewById(R.id.editText);
         EditText e2 = (EditText)findViewById(R.id.editText2);
         TextView t1 = (TextView)findViewById(R.id.textView);
-        int num1 = Integer.parseInt(e1.getText().toString());
-        int num2 = Integer.parseInt(e2.getText().toString());
-        int sum = num1 + num2;
-        t1.setText(Integer.toString(sum));
+        float num1 = Float.parseFloat(e1.getText().toString());
+        float num2 = Float.parseFloat(e2.getText().toString());
+        float sum = num1 + num2;
+        t1.setText(Float.toString(sum));
     }
 
     public void onButton2Click(View v) {
         EditText e1 = (EditText)findViewById(R.id.editText);
         EditText e2 = (EditText)findViewById(R.id.editText2);
         TextView t1 = (TextView)findViewById(R.id.textView);
-        int num1 = Integer.parseInt(e1.getText().toString());
-        int num2 = Integer.parseInt(e2.getText().toString());
-        int multiply = num1 * num2;
-        t1.setText(Integer.toString(multiply));
+        float num1 = Float.parseFloat(e1.getText().toString());
+        float num2 = Float.parseFloat(e2.getText().toString());
+        float multiply = num1 * num2;
+        t1.setText(Float.toString(multiply));
     }
+
+    public void onButton3Click(View v) {
+        EditText e1 = (EditText)findViewById(R.id.editText);
+        EditText e2 = (EditText)findViewById(R.id.editText2);
+        TextView t1 = (TextView)findViewById(R.id.textView);
+        float num1 = Float.parseFloat(e1.getText().toString());
+        float num2 = Float.parseFloat(e2.getText().toString());
+        float divide = num1 / num2;
+        t1.setText(Float.toString(divide));
+    }
+
+    public void onButton4Click(View v) {
+        EditText e1 = (EditText)findViewById(R.id.editText);
+        EditText e2 = (EditText)findViewById(R.id.editText2);
+        TextView t1 = (TextView)findViewById(R.id.textView);
+        float num1 = Float.parseFloat(e1.getText().toString());
+        float num2 = Float.parseFloat(e2.getText().toString());
+        float subtract = num1 - num2;
+        t1.setText(Float.toString(subtract));
+    }
+
 }
